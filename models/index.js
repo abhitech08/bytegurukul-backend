5fe5c3399b6b2d101aee349bcf64676151c89608
Abhijeet Kumar Pandey <abhijeet.kr.pandey.07@gmail.com>
2025-12-26 01:31:09 +0530

    Fix duplicate Sequelize declaration

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
-const { Sequelize } = require("sequelize");
-
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
