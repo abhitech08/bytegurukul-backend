2d68a957749e6707212e3d36480c9b6fc9205812
Abhijeet Kumar Pandey <abhijeet.kr.pandey.07@gmail.com>
2025-12-26 01:24:25 +0530

    Fix Sequelize config to use DATABASE_URL for Render

--- models/index.js
+++ models/index.js
@@ -1,16 +1,22 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false
-  }
-);
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+module.exports = sequelize;
+
 
 const db = {};
 
